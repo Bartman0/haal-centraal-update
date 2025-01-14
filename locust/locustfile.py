9f70d156d85a1af19719aaca43c2126f38209637
Richard Kooijman <kooijman.richard@gmail.com>
2025-01-14 22:21:52 +0100

    added different distribution weights to locust tasks

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -2,19 +2,19 @@ import os
 from locust import FastHttpUser, task, between
 
 TOKEN = os.environ["AZURE_ENTRA_TOKEN"]
+AUTH = {"Authorization": f"Bearer {TOKEN}"}
 
 
 class BrpUpdate(FastHttpUser):
-    wait_time = between(1, 5)
-    auth = {"Authorization": f"Bearer {TOKEN}"}
+    wait_time = between(5, 15)
 
-    @task
+    @task(2)
     def get_volgindicatie(self):
         bsn = 555555026
         with self.rest(
             "GET",
             f"/haalcentraal/api/brpupdate/volgindicaties/{bsn}",
-            headers=self.auth,
+            headers=AUTH,
         ) as resp:
             if resp.js is None:
                 pass  # no need to do anything, already marked as failed
@@ -25,12 +25,12 @@ class BrpUpdate(FastHttpUser):
                     f"'burgerservicenummer' had an unexpected value: {resp.js['burgerservicenummer']}"
                 )
 
-    @task
+    @task(3)
     def get_volgindicaties(self):
         with self.rest(
             "GET",
             "/haalcentraal/api/brpupdate/volgindicaties",
-            headers=self.auth,
+            headers=AUTH,
         ) as resp:
             if resp.js is None:
                 pass  # no need to do anything, already marked as failed
@@ -39,12 +39,12 @@ class BrpUpdate(FastHttpUser):
             elif "burgerservicenummer" not in resp.js["volgindicaties"][0]:
                 resp.failure(f"'burgerservicenummer' missing from response {resp.text}")
 
-    @task
+    @task(5)
     def get_gewijzigde_personen(self):
         with self.rest(
             "GET",
             "/haalcentraal/api/brpupdate/wijzigingen?vanaf=2024-10-01",
-            headers=self.auth,
+            headers=AUTH,
         ) as resp:
             if resp.js is None:
                 pass  # no need to do anything, already marked as failed
